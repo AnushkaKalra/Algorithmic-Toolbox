0ed244b31b57915aab8a88ff92905cd50fd271bc
Anushka Kalra <kalraanushka@gmail.com>
2025-04-14 18:54:40 +0530

    completed week 1 module 1

--- Programming Challenges/Maximum Pairwise Product/maximum_pairwise_product.py
+++ Programming Challenges/Maximum Pairwise Product/maximum_pairwise_product.py
@@ -17,8 +17,23 @@ def max_pairwise_product_naive(numbers):
 def max_pairwise_product(numbers):
     assert len(numbers) >= 2
     assert all(0 <= x <= 2 * 10 ** 5 for x in numbers)
-    type here
 
+    product = 0
+
+    max_num = float('-inf')
+    second_max_num = float('-inf')
+    for i in range(len(numbers)):
+        if numbers[i] > max_num:
+            second_max_num = max_num
+            max_num = numbers[i]
+            max_index = i
+
+    for j in range(len(numbers)):
+        if numbers[j] > second_max_num and j != max_index:  # first and second-largest number may have the same value, but must not have the same index. If they have the same index, then largest and second-largest numbers are same.
+            second_max_num = numbers[j]
+
+    # print(max_num, second_max_num)
+    return max_num * second_max_num
 
 if __name__ == '__main__':
     n = int(input())
